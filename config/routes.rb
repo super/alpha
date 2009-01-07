0f2ffde2c99c07af98e7abf8a1fae76b889e582c
Apple <apple@Rorforge-1.(none)>
2009-01-07 18:04:27 +0800

    New页面
    注册

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.home '', :controller => 'home'
+  map.resources :users
   map.signup '/signup', :controller => 'users', :action => 'new'
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
