5ae79992f9ca0de0bac16755aae569d30afd8f6d
Apple <apple@Rorforge-1.(none)>
2009-01-08 11:51:05 +0800

    login

---
+++ public/javascripts/attributes.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+    var flashvars = {
+myText: "LOG IN"
+    };
+var params = {
+wmode: "transparent"
+};
+var attributes = { };
+swfobject.embedSWF("/new/site_flashden.net/flash/page_title.swf?update=thx", "page_title_content", "460", "26", "9.0.0","expressInstall.swf", flashvars, params, attributes);
+</script>
