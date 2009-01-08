cafba99183941a0acc42f613c51626233db45cf5
Apple <apple@Rorforge-1.(none)>
2009-01-08 14:30:55 +0800

    profect

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,17 +1,19 @@
 class AccountsController < ApplicationController
   def login
     session[:user] = nil
-    user = User.authenticate(params[:username],params[:password])
-    if user
-      self.logged_in_user=user
-      redirect_to home_path
-    else
-      flash[:error] = "用户名／密码错误！"
+    if request.post?
+      user = User.authenticate(params[:username],params[:password])
+      if user
+        self.logged_in_user=user
+        redirect_to home_path
+      else
+        flash[:error] = "用户名／密码错误！"
+      end
     end
   end
   def logout
     session.delete
-    flash[:notice] = "You have Logged out"
+    flash[:notice] = "您已经退出了！"
     redirect_to login_path
   end
 end
---
+++ public/javascripts/swf.js
@@ -0,0 +1,2 @@
+<script type="text/javascript"> var flashvars = {}; var params = { wmode: "transparent" }; var attributes = { }; swfobject.embedSWF("../images/image/homepage.swf", "flash_item_container", "341", "214", "9.0.0","expressInstall.swf", flashvars, params, attributes);
+  </script>
